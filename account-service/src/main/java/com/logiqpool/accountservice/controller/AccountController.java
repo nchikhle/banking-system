33da2d065393d9723f532669fe7f0eb6c0e99745
Namrata Chikhle <namrata.chikhle@gmail.com>
2026-05-10 16:53:46 +0100

    feat(account-service): add getAccount and updateBalance endpoints

--- account-service/src/main/java/com/logiqpool/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/logiqpool/accountservice/controller/AccountController.java
@@ -5,17 +5,17 @@ import com.logiqpool.accountservice.dto.AccountResponseDto;
 
 import com.logiqpool.accountservice.service.AccountService;
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("api/v1/accounts")
+@RequestMapping("api/v1/accounts/")
 @Slf4j
 public class AccountController {
 
@@ -29,8 +29,29 @@ public class AccountController {
     @PostMapping
     public ResponseEntity<AccountResponseDto> createAccount(@Valid  @RequestBody AccountRequestDto request) {
         // The @Valid annotation triggers the rules we set in the DTO (like @DecimalMin)
-        log.info("Creating account...");
+        log.info("REST request to create account");
         //return service.createAccount(account);
         return new ResponseEntity<>(accountService.createAccount(request), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{accountNumber}")
+    public ResponseEntity<AccountResponseDto> getAccountDetails(@PathVariable String accountNumber){
+        log.info("REST request to get account: {}", accountNumber);
+        AccountResponseDto account = accountService.getAccount(accountNumber);
+        log.debug("Account Details: {}", account);
+
+        return ResponseEntity.ok(account);
+    }
+
+    /**
+     * This endpoint will be called by your Transaction Service (via Feign)
+     * to adjust balances during a transfer.
+     */
+    @PutMapping("/{accountNumber}/balance")
+    public ResponseEntity<AccountResponseDto> updateBalance(@PathVariable String accountNumber,
+                                                            @RequestParam BigDecimal amount){
+        log.info("REST request to update balance for: {}", accountNumber);
+        return ResponseEntity.ok(accountService.updateBalance(accountNumber, amount));
+
     }
 }
--- account-service/src/main/java/com/logiqpool/accountservice/dto/AccountRequestDto.java
+++ account-service/src/main/java/com/logiqpool/accountservice/dto/AccountRequestDto.java
@@ -1,46 +1,37 @@
 package com.logiqpool.accountservice.dto;
+
+import com.logiqpool.accountservice.model.AccountType;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.logiqpool.accountservice.model.AccountType;
-/***
- * A Security & Logic Note: The client should never send the accountNumber when creating an account. The bank's system generates that. If you let the user provide it, they could guess existing numbers or break your internal sequencing.
- *
- * Also, for a banking API, we must enforce strict validation. We don't want an account created with a balance of -5,000 or a null name.
- */
+import lombok.Builder;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder // Useful for creating objects in tests
 //@JsonIgnoreProperties(ignoreUnknown = false)
-public class AccountRequestDto {
+public record AccountRequestDto(
 
-    //@Column(unique = true, nullable = false, length = 20)
+        //@Column(unique = true, nullable = false, length = 20)
 
-    //private String accountNumber;
+        //private String accountNumber;
 
-    //@Column(nullable = false)
-    @NotBlank(message ="Account holder name is required")
-    private String accountHolderName;
+        //@Column(nullable = false)
+        @NotBlank(message = "Account holder name is required")
+        String accountHolderName,
 
-    //@Column(nullable = false,precision = 19, scale = 4)
-    @NotNull(message = "Initial deposite is required")
-    @DecimalMin(value = "0.0", inclusive = true, message = "Balance cannot be negative")
-    private BigDecimal balance;
+        //@Column(nullable = false,precision = 19, scale = 4)
+        @NotNull(message = "Initial deposite is required")
+        @DecimalMin(value = "0.0", inclusive = true, message = "Balance cannot be negative")
+        BigDecimal balance,
 
-   // @Column(nullable = false)
-    @Size(min = 3,max = 3, message = "Currency must be a 3-letter ISO code")
-    private String currency; //e.g "USD
+        // @Column(nullable = false)
+        @Size(min = 3, max = 3, message = "Currency must be a 3-letter ISO code")
+        String currency, //e.g "USD
 
-    @NotNull
-    private AccountType accountType; // "SAVINGS" or "CHECKING"
+        @NotNull
+        AccountType accountType // "SAVINGS" or "CHECKING")
 
+) {
 }
